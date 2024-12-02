a5d903ba99982eb174c3708184885e9a40365514
shohan2032 <mohammad.shohan2032@gmail.com>
2024-12-02 13:53:26 +0600

    Just updated nginx configuration

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-# Load environment variables
-echo "Loading environment variables..."
-export $(grep -v '^#' /etc/nginx/.env | xargs)
+SSL_DIR="/etc/nginx/ssl"
+DOMAIN="shohan.dev"
 
-# echo "Checking SSL directory: $SSL_DIR"
-# if [ ! -d "$SSL_DIR" ]; then
-#     echo "SSL directory does not exist. Creating it..."
-#     mkdir -p "$SSL_DIR"
-# fi
+# Ensure the SSL directory exists
+if [ ! -d "$SSL_DIR" ]; then
+    echo "SSL directory does not exist. Creating it..."
+    mkdir -p "$SSL_DIR"
+fi
 
 # Check if certificates exist; if not, generate them
 if [ ! -f "$SSL_DIR/shohan.dev.crt" ] || [ ! -f "$SSL_DIR/shohan.dev.key" ]; then
-    echo "Generating SSL certificates..."
-    mkcert -install
-    mkcert -cert-file $SSL_DIR/shohan.dev.crt -key-file $SSL_DIR/shohan.dev.key $DOMAIN
+    echo "Generating SSL certificates with OpenSSL..."
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout "$SSL_DIR/shohan.dev.key" \
+        -out "$SSL_DIR/shohan.dev.crt" \
+        -subj "/CN=$DOMAIN"
     echo "Certificates generated at $SSL_DIR"
 else
     echo "SSL certificates already exist. Skipping generation."
